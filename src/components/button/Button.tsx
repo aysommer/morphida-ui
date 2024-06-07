2c4dae859ff2f8babea9bcdc6381150cceda02ec
Artyom Sommer <litovskys@gmail.com>
2024-06-07 18:00:41 +0300

    Storybook init.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,19 @@
 import type { ButtonProps } from "./types";
+import { clsx } from "clsx";
+
+import "./Button.css";
 
 /**
  * Base button.
  */
-const Button: React.FC<ButtonProps> = ({ children, ...other }) => {
-   return <button {...other}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ children, className, ...other }) => {
+   const computedClassName = clsx("mphd-button", className);
+
+   return (
+      <button className={computedClassName} {...other}>
+         {children}
+      </button>
+   );
 };
 
 export default Button;
